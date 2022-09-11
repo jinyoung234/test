32ab46dc912aaf4e326ba3d7e5260584451e8c21
jinyoung <sm099@naver.com>
2022-09-11 16:00:28 +0900

    feat : summary page test code implement

--- src/pages/SummaryPage/test/Summary.test.ts
+++ src/pages/SummaryPage/test/Summary.test.ts
@@ -3,7 +3,15 @@ import '@testing-library/jest-dom/extend-expect';
 import SummaryPage from '../SummaryPage';
 
 describe('summary page', () => {
-  test('test App component initialize value', () => {
+  test('check box and button initial state test', () => {
     render(SummaryPage());
+    const checkBox = screen.getByRole('checkbox', {
+      name: '주문하려는 것을 확인하셨나요?',
+    });
+    expect(checkBox).not.toBeChecked();
+    const confirmButton = screen.getByRole('button', {
+      name: '주문 확인',
+    });
+    expect(confirmButton).toBeDisabled();
   });
 });
