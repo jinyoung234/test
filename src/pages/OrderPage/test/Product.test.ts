76c02227bcecfe8d7bcf248c1db6e4f8056612d1
jinyoung <sm099@naver.com>
2022-09-13 17:33:02 +0900

    feat : fetching Error test code

--- src/pages/OrderPage/test/Product.test.ts
+++ src/pages/OrderPage/test/Product.test.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
 import OrderComponent from '../../../components/OrderPage/OrderComponent';
+import { server } from '../../../utils/mocks/server';
 import { mockServer } from '../../../utils/mocks/setUpTest';
 
 describe('Product component test', () => {
@@ -11,5 +13,17 @@ describe('Product component test', () => {
       name: /product/i,
     });
     expect(productImages).toHaveLength(2);
+  });
+  test('image fetching error from server', async () => {
+    server.resetHandlers(
+      rest.get('http://localhost:5000/products', (req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+    render(OrderComponent());
+    const errorComponent = await screen.findByText(
+      'network 에러가 발생했습니다.'
+    );
+    expect(errorComponent).toHaveTextContent('network 에러가 발생했습니다.');
   });
 });
