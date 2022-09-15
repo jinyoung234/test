80abc87b7e26928ff4bd7c01a9000ee0aa94d9d8
jinyoung <sm099@naver.com>
2022-09-15 21:42:36 +0900

    feat : Total component combine OrderContext logic

--- src/components/OrderPage/Total.tsx
+++ src/components/OrderPage/Total.tsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
+import { OrderContext } from '../../utils/contexts/OrderContext';
+
 function Total(): JSX.Element {
+  const orderObject = useContext(OrderContext);
+  const totalPrice = orderObject[0]?.totals;
   return (
     <div style={{ width: '50%' }}>
-      <h2 style={{ marginBottom: '40px' }}>Total Price : </h2>
+      <h2 style={{ marginBottom: '40px' }}>
+        Total Price : {totalPrice?.total}
+      </h2>
       <button>주문</button>
     </div>
   );
